09bbed43d7b60a75fe3795d3679a832a7869e621
Leon Timmermans <fawaka@gmail.com>
2013-09-09 13:47:47 +0200

    Bumped version to 0.002

--- lib/App/ModuleBuildTiny.pm
+++ lib/App/ModuleBuildTiny.pm
@@ -3,7 +3,7 @@ package App::ModuleBuildTiny;
 use 5.008;
 use strict;
 use warnings FATAL => 'all';
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 use Exporter 5.57 'import';
 our @EXPORT = qw/modulebuildtiny/;
@@ -159,7 +159,7 @@ App::ModuleBuildTiny - A standalone authoring tool for Module::Build::Tiny
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 FUNCTIONS
 
