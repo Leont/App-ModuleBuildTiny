1354ddc66d6adabad16cb62775392dee4b330cd1
Leon Timmermans <fawaka@gmail.com>
2016-05-17 10:29:27 +0200

    Bump to version 0.013

--- lib/App/ModuleBuildTiny.pm
+++ lib/App/ModuleBuildTiny.pm
@@ -3,7 +3,7 @@ package App::ModuleBuildTiny;
 use 5.010;
 use strict;
 use warnings;
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 use Exporter 5.57 'import';
 our @EXPORT = qw/modulebuildtiny/;
@@ -306,7 +306,7 @@ App::ModuleBuildTiny - A standalone authoring tool for Module::Build::Tiny
 
 =head1 VERSION
 
-version 0.012
+version 0.013
 
 =head1 DESCRIPTION
 
