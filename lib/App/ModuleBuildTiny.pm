0817bb7bf582c0d11987edd65e1c88dfe0fe7bc5
Leon Timmermans <fawaka@gmail.com>
2019-01-26 21:00:57 +0100

    Bump version to 0.026

--- lib/App/ModuleBuildTiny.pm
+++ lib/App/ModuleBuildTiny.pm
@@ -3,7 +3,7 @@ package App::ModuleBuildTiny;
 use 5.010;
 use strict;
 use warnings;
-our $VERSION = '0.025';
+our $VERSION = '0.026';
 
 use Exporter 5.57 'import';
 our @EXPORT = qw/modulebuildtiny/;
@@ -282,7 +282,7 @@ App::ModuleBuildTiny - A standalone authoring tool for Module::Build::Tiny
 
 =head1 VERSION
 
-version 0.025
+version 0.026
 
 =head1 DESCRIPTION
 
--- lib/App/ModuleBuildTiny/Dist.pm
+++ lib/App/ModuleBuildTiny/Dist.pm
@@ -3,7 +3,7 @@ package App::ModuleBuildTiny::Dist;
 use 5.010;
 use strict;
 use warnings;
-our $VERSION = '0.025';
+our $VERSION = '0.026';
 
 use CPAN::Meta;
 use Carp qw/croak/;
