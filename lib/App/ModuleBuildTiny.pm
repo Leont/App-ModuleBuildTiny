e15d92acc7178321214e5c7daa1cbb2675a93cff
Leon Timmermans <fawaka@gmail.com>
2023-02-05 17:05:51 +0100

    v0.033
              - Add xt test running to test command
              - Add documentation for --commit, --tag and --push

--- lib/App/ModuleBuildTiny.pm
+++ lib/App/ModuleBuildTiny.pm
@@ -3,7 +3,7 @@ package App::ModuleBuildTiny;
 use 5.014;
 use strict;
 use warnings;
-our $VERSION = '0.032';
+our $VERSION = '0.033';
 
 use Exporter 5.57 'import';
 our @EXPORT = qw/modulebuildtiny/;
--- lib/App/ModuleBuildTiny/Dist.pm
+++ lib/App/ModuleBuildTiny/Dist.pm
@@ -3,7 +3,7 @@ package App::ModuleBuildTiny::Dist;
 use 5.014;
 use strict;
 use warnings;
-our $VERSION = '0.032';
+our $VERSION = '0.033';
 
 use CPAN::Meta;
 use Config;
